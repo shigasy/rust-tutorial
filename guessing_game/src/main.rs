e1d576c5c85b56d011768ae87996108c5a74b5cb
shigasy <kxx555u.u@gmail.com>
2020-06-28 23:00:54 +0900

    add: ユーザー入力読み込み出力

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    // &で参照するおかげでデータを複数回メモリにコピーせずとも同じデータにアクセスできる
+    // 安全かつ簡単
+
+    // read_lineの返却値はio::Result
+    // もし、Resultにexpectメソッドを使用していなければ、コンパイルエラーを示す
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess)
 }
